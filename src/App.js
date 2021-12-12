73a2962100bf141561c1848e82da721b54b9eb4f
retater56 <70779585+retater56@users.noreply.github.com>
2021-12-12 20:00:05 +0300

    fix: delete unused files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import Header from './components/header/Header';
+import Main from './components/main/Main';
 
 import './sass/style.scss';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>My React App!</h1>
+      <div className='body__wrapper'>
+        <Header />
+        <Main />
       </div>
     );
   }
