ed264092cdcfd06b5f9f460f93fed5992909b9fc
retater56 <70779585+retater56@users.noreply.github.com>
2021-12-26 22:55:47 +0300

    feat: create form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Form from './components/form/form';
 
 import './sass/style.scss';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className='body__wrapper'>
+        <Form />
       </div>
     );
   }
