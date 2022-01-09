4579cd7b9f70f0b248bdacd743693ea78f7a9ed5
retater56 <70779585+retater56@users.noreply.github.com>
2022-01-09 17:25:35 +0300

    init: start task with reactApi files

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# reactStudy2021
+# React + Webpack + Bootstrap
+
+## Task 
+[React Router](https://github.com/rolling-scopes-school/tasks/blob/master/tasks/react/react-api.md)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Search from "./components/Search";
+import './app.scss'
+
+const App = () => {
+  return (
+    <Search />
+  )
+};
+
+export default App;
