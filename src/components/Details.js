3755cf70a071394f1b92a3cd03af7eff021d6f46
retater56 <70779585+retater56@users.noreply.github.com>
2022-01-15 15:07:00 +0300

    feat: create details page

---
+++ src/components/Details.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import { Container} from "react-bootstrap";
+import { useParams } from 'react-router-dom';
+
+const Details = () => {
+  const [state, setState] = useState(null);
+  const [error, setError] = useState(null);
+  const params = useParams();
+
+  const [title, farm, server, id, secret] = params.id.split('-');
+
+  useEffect(() => {
+    fetchingData()
+  })
+
+  const fetchingData = () => {
+    fetch(
+      `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}.jpg`
+    )
+      .then((res) => {
+        console.log(res)
+        if (!res.ok) {
+          throw Error("could not fetch data from that resource");
+        }
+        setState(`https://farm${farm}.staticflickr.com/${server}/${id}_${secret}.jpg`);
+      })
+      .catch((err) => {
+        setError(err.message);
+      });
+  };
+
+  return (
+
+    <Container>
+      <h2>{title}</h2>
+      <h3>Original link: <a href={state} target='_blank'>{state}</a></h3>
+      <img alt={`${title}`} src={state}></img>
+    </Container>
+  )
+}
+
+export default Details;
